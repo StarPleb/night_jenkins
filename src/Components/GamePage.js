c972176cb1d5ce82351984f20e9965c20ae7e05b
StarPleb <85961145+StarPleb@users.noreply.github.com>
2022-04-01 11:57:18 -0500

    Added onClick function for each cell in map.
    Successfully changes the classname of the clicked element, but the css styling of that element does not update. Need to figure out why!

--- src/Components/GamePage.js
+++ src/Components/GamePage.js
@@ -5,11 +5,15 @@ export default function GamePage() {
 
     const [map, setMap] = useState([[]])
     const [tokens, setTokens] = useState([[]])
+    const [counter, setCounter] = useState(0)
 
     useEffect(()=>{
         console.log("Hello gaming world!")
         setMap(init_map(15, 15))
         setTokens(init_tokens(15, 15))
+// Add event listener to table
+        const el = document.getElementById("outside");
+        el.addEventListener("click", modifyText, false);
         
     }, [])
 
@@ -41,14 +45,41 @@ export default function GamePage() {
         }
         return temp
     }
-    
+
+    function modifyText() {
+        const t2 = document.getElementById("t2");
+        if (t2.firstChild.nodeValue === "three") {
+            t2.firstChild.nodeValue = "two";
+        } else {
+            t2.firstChild.nodeValue = "three";
+        }
+    }
+
+    function modifyCell(e) {
+        let type = e.target.className
+        console.log("Hey world!", e.target)
+        if(type === "Cell-alive"){
+            type = "Cell-dead"
+        }
+        else{
+            type = "Cell-alive"
+        }
+
+        e.target.className = type
+    }
+
+// Add event listener to table
+
     const map_array_mapped = map.map((row, index)=>{
+        let new_index_array = []
         return (
             <div className={"Grid-row"}>
                 {row.map((column, idx)=>{
+                    let new_index = index*idx + idx
+                    new_index_array.push(new_index)
                     console.log(index, idx)
                     return (
-                        <div className={"Grid-cell"}/>
+                        <div className={"Cell-dead"} id={`${new_index}`} onClick={modifyCell}/>
                     )
                 })}
             </div>
@@ -57,10 +88,14 @@ export default function GamePage() {
     })
 
     return(
-        <div>
+        <div className={"Game-page"}>
             <h1>Let's play!</h1>
             <div className={"Grid-view"}>
                 {map_array_mapped}
             </div>
+            <table id="outside">
+                <tr><td id="t1">one</td></tr>
+                <tr><td id="t2">two</td></tr>
+            </table>
         </div>
     );}
